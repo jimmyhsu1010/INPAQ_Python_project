ad0f66014be532e7088eaba4016022ae16c79f35
jimmyhsu1010 <jimmyhsu1010@gmail.com>
2020-11-10 22:49:43 +0800

    WIP每月報告新增

--- 數據整理/sorting_test.py
+++ 數據整理/sorting_test.py
@@ -1,14 +1,18 @@
 
 import random
-x = []
+# x = random.sample(range(10000), k=30)
 
-while len(x) <= 10:
-    x.append(random.randint(0, 30))
+x = [1, 2, 4, 5, 6, 8, 7, 9, 3]
 
-for i in range(len(x)-1, -1, -1):
-    for j in range(len(x)-1, -1, -1):
-        if x[j] < x[i]:
-            x[j], x[i] = x[i], x[j]
+# for i in range(len(x)-1, -1, -1): # 升冪
+#     for j in range(len(x)-1, -1, -1):
+#         if x[j] < x[i]:
+#             x[j], x[i] = x[i], x[j]
+
+for i in range(len(x)): # 降冪
+    for j in range(len(x)):
+        if x[i] > x[j]:
+            x[i], x[j] = x[j], x[i]
 
 
 print(x)

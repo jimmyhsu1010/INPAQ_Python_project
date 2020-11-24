579f50c6453a220153a3deff8cd60c8898324c03
jimmyhsu1010 <jimmyhsu1010@gmail.com>
2020-11-24 23:21:05 +0800

    dash pivottable修正完畢

--- Dashboard/sales_pivot.py
+++ Dashboard/sales_pivot.py
@@ -15,9 +15,15 @@ auth = dash_auth.BasicAuth(
     VALID_USERNAME_PASSWORD_PAIRS
 )
 
-sales = pd.read_excel(r'C:\Users\kaihsu\Desktop\業績總表\Weekly report_v0.1.xlsx', sheet_name='出貨明細')
-mon_order=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-sales['預交月份'] = pd.Categorical(sales['預交月份'], ordered=True, categories=mon_order)
+sales = pd.read_excel('/Users/kai/Desktop/Weekly report_v0.1.xlsx', sheet_name='出貨明細')
+sales['BG'] = sales.apply(lambda x: 'RFBU2' if 'RFDP' in x['品名'] else 'RFBU1' if 'RFDP' not in x['品名'] and 'RF' in x['BG'] else x['BG'], axis=1)
+sales = sales[sales['狀態'].str.contains('出')]
+sales = sales[['BG', 'Group', '銷售單號', '開單日期', '預交日期', '預交年份', '預交月份', '負責業務', '品名', '幣別', '單價', '數量', '本國幣別NTD', '客戶料號', 'Term']]
+mon_dict = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
+sales['數量'] = sales['數量'].astype('int')
+sales['本國幣別NTD'] = sales['本國幣別NTD'].astype('int')
+sales['預交月份'] = sales['預交月份'].map(mon_dict)
+
 data = sales.values.tolist()
 data.insert(0, sales.columns.tolist())
 

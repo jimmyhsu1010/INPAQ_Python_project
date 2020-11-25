fce14a383a4c89415b03def1a54acbc4c5f625aa
jimmyhsu1010 <jimmyhsu1010@gmail.com>
2020-11-25 18:01:36 +0800

    Deployed on Heruko

--- Dashboard/sales_pivot.py
+++ Dashboard/sales_pivot.py
@@ -6,16 +6,21 @@ import dash_auth
 
 # Keep this out of source code repository - save in a file or a database
 VALID_USERNAME_PASSWORD_PAIRS = {
-    'kaihsu': '711010'
+    'kaihsu': '711010', 'yiwen': '9487', 'amber': '12345678', 'joycechen': 'abcd1234', 'guest': 'inpaq1234'
 }
 
 app = dash.Dash(__name__)
+
+# Heroku用
+server = app.server
+
 auth = dash_auth.BasicAuth(
     app,
     VALID_USERNAME_PASSWORD_PAIRS
 )
 
-sales = pd.read_excel('/Users/kai/Desktop/Weekly report_v0.1.xlsx', sheet_name='出貨明細')
+# sales = pd.read_excel('/Users/kai/Desktop/Weekly report_v0.1.xlsx', sheet_name='出貨明細') # Mac用
+sales = pd.read_excel("Weekly report_v0.1.xlsx", sheet_name='出貨明細') # Heroku用
 sales['BG'] = sales.apply(lambda x: 'RFBU2' if 'RFDP' in x['品名'] else 'RFBU1' if 'RFDP' not in x['品名'] and 'RF' in x['BG'] else x['BG'], axis=1)
 sales = sales[sales['狀態'].str.contains('出')]
 sales = sales[['BG', 'Group', '銷售單號', '開單日期', '預交日期', '預交年份', '預交月份', '負責業務', '品名', '幣別', '單價', '數量', '本國幣別NTD', '客戶料號', 'Term']]
@@ -43,7 +48,8 @@ app.layout = html.Div(
         cols=['預交月份'],
         rows=['預交年份', '負責業務'],
         vals=['本國幣別NTD'],
-        aggregatorName='Sum'
+        aggregatorName='Sum',
+        menuLimit=10000
     )
 )
 

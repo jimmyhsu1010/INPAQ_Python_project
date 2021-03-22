2aa53cb2a01191bb952201b9b96e386445b2767c
jimmyhsu1010 <jimmyhsu1010@gmail.com>
2021-03-23 00:03:45 +0800

    mouser網站爬取無法突破封鎖……

--- 網路爬蟲/crawler.py
+++ 網路爬蟲/crawler.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 import time
 import random
+import csv
 
 pd.set_option("display.max_columns", None)
 
@@ -25,7 +26,7 @@ while page <= 4482:
     datas = selector.xpath("//div/div[1]/div[6]/div/form/div[2]/div[2]/table/tbody/tr")
     for data in datas:
         pn = data.xpath("td[3]/div[1]/a")[0].text
-        pn_ref = data.xpath("//a[contains(@id, 'lnkMfrPartNumber')]/@href")[0]
+        pn_ref = data.xpath("td[3]/div[1]/a/@href")[0]
         pn_detail = pn_url + pn_ref
         r = requests.get(pn_detail, headers=headers)
         sel = etree.HTML(r.content)
@@ -48,6 +49,7 @@ while page <= 4482:
             else:
                 val.append(v)
         detail_dict = dict(zip(k, val))
+        print(detail_dict)
         manufacturer = data.xpath("td[4]/a")[0].text
         description = data.xpath("td[5]/span")[0].text
         ganyingxishu = data.xpath("td[11]/span")[0].text
@@ -55,14 +57,19 @@ while page <= 4482:
         zuidazhiliudian = data.xpath("td[13]/span")[0].text
         zhongduanleixing = data.xpath("td[14]/span")[0].text
         zuigaogongzuowendu = data.xpath("td[15]/span")[0].text
-        # print(pn, manufacturer, description, ganyingxishu, naishouxing, zuidazhiliudian, zhongduanleixing,
-        #       zuigaogongzuowendu)
-        df = df.append({"品名": pn, "製造商": manufacturer, "描述": description, "感應係數": ganyingxishu, "耐受性": naishouxing,
-                        "最大直流電流": zuidazhiliudian, "終端類型": zhongduanleixing,
-                        "最高工作溫度": zuigaogongzuowendu, "產品類型": detail_dict["產品類型:"],
-                        "長度": detail_dict["長度:"], "寬度": detail_dict["寬度:"],
-                        "高度": detail_dict["高度:"], "原廠包裝數量": detail_dict["原廠包裝數量:"]}, ignore_index=True)
-        time.sleep(random.randint(0, 1))
+        if "長度:" and "寬度:" and "高度:" not in detail_dict.keys():
+            df = df.append({"品名": pn, "製造商": manufacturer, "描述": description, "感應係數": ganyingxishu, "耐受性": naishouxing,
+                            "最大直流電流": zuidazhiliudian, "終端類型": zhongduanleixing,
+                            "最高工作溫度": zuigaogongzuowendu, "產品類型": detail_dict["產品類型:"],
+                            "長度": "N/A", "寬度": "N/A",
+                            "高度": "N/A", "原廠包裝數量": detail_dict["原廠包裝數量:"]}, ignore_index=True)
+        else:
+            df = df.append({"品名": pn, "製造商": manufacturer, "描述": description, "感應係數": ganyingxishu, "耐受性": naishouxing,
+                            "最大直流電流": zuidazhiliudian, "終端類型": zhongduanleixing,
+                            "最高工作溫度": zuigaogongzuowendu, "產品類型": detail_dict["產品類型:"],
+                            "長度": detail_dict["長度:"], "寬度": detail_dict["寬度:"],
+                            "高度": detail_dict["高度:"], "原廠包裝數量": detail_dict["原廠包裝數量:"]}, ignore_index=True)
+        time.sleep(random.randint(0, 3))
     print(df)
 
     # if selector.xpath("//a[@id='lnkPager_lnkNext']"):
@@ -82,7 +89,7 @@ else:
     print("爬取完畢")
 
 # Mac電腦使用下面路徑
-# df.to_excel("/Users/kai/Desktop/mouser電感.xlsx", index=False)
+df.to_excel("/Users/kai/Desktop/mouser電感.xlsx", index=False)
 
 # Windows使用下面路徑
-df.to_excel(r"C:\Users\kaihsu\Desktop\mouser電感.xlsx", index=False)
+# df.to_excel(r"C:\Users\kaihsu\Desktop\mouser電感.xlsx", index=False)

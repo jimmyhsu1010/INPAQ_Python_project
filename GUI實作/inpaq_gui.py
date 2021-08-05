9a7246dd533f1124fbee6b9f026dbdaca9ba41e5
jimmyhsu1010 <jimmyhsu1010@gmail.com>
2021-08-06 00:20:06 +0800

    20210806 INPAQ小幫手0.1版修改icon

--- GUI實作/inpaq_gui.py
+++ GUI實作/inpaq_gui.py
@@ -1,22 +1,25 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 from sales import Sales
 
 # 主視窗建立
 win = tk.Tk()
 win.title('INPAQ工作小幫手')
-win.geometry('600x450+1200+50')
+win.geometry('650x450+1200+50')
 win.resizable(False, False)
 # win.attributes('-topmost', True)
 
 # 視窗內介面建立
 img = tk.PhotoImage(file='inpaq_logo.png')
+img = img.subsample(2, 2)
 imagelabel = tk.Label(win, image=img)
-imagelabel.grid(row=0, column=0, columnspan=3, sticky=tk.E)
+imagelabel.grid(row=0, column=2, columnspan=3, sticky=tk.E)
 
-sales_button = tk.Button(win, text='業務專區', command=Sales)
+sales_icon = tk.PhotoImage(file='sales_icon.png')
+sales_icon = sales_icon.subsample(6, 6)
+sales_button = tk.Button(win, text='業務專區', image=sales_icon, compound= tk.LEFT, command=Sales, bg='blue', width=8, font='微軟正黑體 20') # compound用來讓文字和圖片對齊
 sales_button.grid(row=1, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=40, pady=5)
-sales_button.config(width=6, height=4, font='微軟正黑體 20')
+# sales_button.config(width=6, height=4, font='微軟正黑體 20')
 
 cs_button = tk.Button(win, text='CS專區')
 cs_button.grid(row=1, column=1, sticky=tk.W+tk.E+tk.N+tk.S, padx=40, pady=5)
